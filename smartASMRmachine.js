0a6344115c49abe0e80333d80a352a931904bef3
Amreen <amreenashraf@gmail.com>
2018-09-17 06:50:53 -0400

    commit #3

---
+++ smartASMRmachine.js
@@ -0,0 +1,15 @@
+var h1;
+var h2;
+
+function setup(){
+    h1= createElement('h1','Click me to learn more!');
+    //h1.position(400,600);
+    
+    
+}
+
+function mousePressed(){
+    h1.html("My name is SAM. I am a smart ASMR machine.");
+    createP("I Listen. I learn. Evenetually I will learn to create my own sounds. It will help me understand you while I learn about myself.I was created at OCADU as a project for the Digital Futures grad class Explorations DIGF-6909-001.");
+   
+}
