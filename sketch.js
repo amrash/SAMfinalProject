528b977ff70958ce9349782c2fa863486bdefa65
Amreen <amreenashraf@gmail.com>
2018-09-17 03:05:35 -0400

    commit #2

--- sketch.js
+++ sketch.js
@@ -1,10 +1,7 @@
+var button;
 
-var canvas;
-
-function setup() {
-    //canvas = createCanvas(1600, 400);
-   //canvas.position(0,1000);
+function setup(){
+button=createButton("save");
+    
     
 }
-
-
