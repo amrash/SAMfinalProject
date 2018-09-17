5a459617eef2f29701249334d1f324e76fe402b8
Amreen <amreenashraf@gmail.com>
2018-09-17 00:50:55 -0400

    Final project commit #1
    Final Project begining

--- sketch.js
+++ sketch.js
@@ -2,11 +2,9 @@
 var canvas;
 
 function setup() {
- canvas = createCanvas(1600, 400);
- //canvas.position(0,1000);
-}
-function draw() {
-    background(125);
-    fill(155,0,0);
+    //canvas = createCanvas(1600, 400);
+   //canvas.position(0,1000);
     
 }
+
+
