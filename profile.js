a31c8d83c13ffe05fdc8707b7c4166d87571691d
Amreen <amreenashraf@gmail.com>
2018-09-17 10:54:55 -0400

    Commit 5
    added javascript

---
+++ profile.js
@@ -0,0 +1,13 @@
+var button;
+
+
+function setup(){
+    button= createButton('UPLOAD DEVICE...!');
+    //h1.position(400,600);
+    
+    
+}
+
+function mousePressed(){
+    createP("Your Apple Watch has been uploaded");
+}
